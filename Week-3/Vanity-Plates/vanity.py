a0fedac33587db248fe91d447a0caa45bbb004c1
Munazar-99 <munazar.ali.001@gmail.com>
2023-07-31 23:30:01 +0200

    solve vanity plate excercise

--- Week-3/Vanity-Plates/vanity.py
+++ Week-3/Vanity-Plates/vanity.py
@@ -9,7 +9,7 @@ def main():
 def is_valid(s):
     first_two_letters = s[0:2]
 
-    if first_two_letters.isnumeric() or len(s)>  6 or len(s) < 2:
+    if first_two_letters.isnumeric() or len(s)>  6 or len(s) < 2 or s[-1].isnumeric() == False:
         return False
     return True
     
