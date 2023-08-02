07e95ad75c13dfd174143954642bc57a251be4c7
Munazar-99 <munazar.ali.001@gmail.com>
2023-08-02 23:00:43 +0200

    add question pdf  for vanity plates

--- Week-3/Vanity-Plates/vanity.py
+++ Week-3/Vanity-Plates/vanity.py
@@ -7,10 +7,30 @@ def main():
 
 
 def is_valid(s):
+    s=s.lower()
     first_two_letters = s[0:2]
-
-    if first_two_letters.isnumeric() or len(s)>  6 or len(s) < 2 or s[-1].isnumeric() == False:
+    if first_two_letters.isnumeric() or len(s) >  6 or len(s) < 2 or not s[-1].isnumeric()  or  is_first_int_zero(s) or is_special_char(s):
         return False
     return True
-    
+
+def is_first_int_zero(s):
+    check = False
+    for char in s:
+        if char.isnumeric():
+            if char == '0':
+                check = True
+                break
+            else:
+                break
+    return check
+def is_special_char(s):
+    check = False
+    for char in s:
+        if char.isnumeric() or char.isalpha():
+            continue
+        else:
+            check = True 
+            break
+    return check
+
 main()
