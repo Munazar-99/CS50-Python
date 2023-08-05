eeb669c5b62f4df8c692a530fc6d1d893449a24f
Munazar-99 <munazar.ali.001@gmail.com>
2023-08-05 23:34:15 +0200

    refactor vanity plates  exercise

--- Week-3/Problem-Set-3/Vanity-Plates/vanity.py
+++ Week-3/Problem-Set-3/Vanity-Plates/vanity.py
@@ -8,8 +8,12 @@ def main():
 
 def is_valid(s):
     s=s.lower()
-    first_two_letters = s[0:2]
-    if first_two_letters.isnumeric() or len(s) >  6 or len(s) < 2 or not s[-1].isnumeric()  or  is_first_int_zero(s) or is_special_char(s):
+    first_two_letters = s[:2]
+    if (first_two_letters.isnumeric()
+        or  6 < len(s) < 2
+        or not s[-1].isnumeric()  
+        or  is_first_int_zero(s) 
+        or is_special_char(s)):
         return False
     return True
 
@@ -26,7 +30,7 @@ def is_first_int_zero(s):
 def is_special_char(s):
     check = False
     for char in s:
-        if char.isnumeric() or char.isalpha():
+        if char.isalnum():
             continue
         else:
             check = True 
