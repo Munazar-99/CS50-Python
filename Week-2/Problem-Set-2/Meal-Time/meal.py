6efac07c4ec33261ded523b23a127e6d92a8249c
Munazar-99 <munazar.ali.001@gmail.com>
2023-08-05 23:34:40 +0200

    refactor meal time  exercise

--- Week-2/Problem-Set-2/Meal-Time/meal.py
+++ Week-2/Problem-Set-2/Meal-Time/meal.py
@@ -1,19 +1,18 @@
 def main():
     time_given = input('What time is it? ')
     numerized_time = convert(time_given)
-    match numerized_time:
-        case _ if numerized_time >= 7.0 and numerized_time <= 8.0:
-            print('its breakfast time')
-        case _ if numerized_time >= 12.00 and numerized_time <= 13.00:
-            print('its lunch time')
-        case _ if numerized_time >= 18.00 and numerized_time <= 19.00:
-            print('its dinner time')
-        case _:
-            print('Not meal time')
+    
+    if 7.0 <= numerized_time <= 8.0:
+        print('It\'s breakfast time')
+    elif 12.0 <= numerized_time <= 13.0:
+        print('It\'s lunch time')
+    elif 18.0 <= numerized_time <= 19.0:
+        print('It\'s dinner time')
+    else:
+        print('Not meal time')
 
 def convert(time):
     return float(time.replace(':', '.'))
-
 
 if __name__ == "__main__":
     main()
